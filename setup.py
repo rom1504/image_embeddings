5b65767bea540abb2b9a048f47702e741bb30f34
Romain Beaumont <romain.rom1@gmail.com>
2020-07-18 22:01:29 +0200

    add missing dependencies

--- setup.py
+++ setup.py
@@ -30,6 +30,8 @@ if __name__ == "__main__":
             "efficientnet",
             "faiss-cpu-noavx2",
             "dataclasses",
+            "ipython",
+            "ipywidgets",
         ],
         tests_require=["pytest", "black"],
         dependency_links=[],
