9bf3e981c04ec51c5d22f375acb1725eca5cfb09
Romain Beaumont <romain.rom1@gmail.com>
2020-07-18 23:03:44 +0200

    Release 1.3.1

--- HISTORY.md
+++ HISTORY.md
@@ -1,3 +1,7 @@
+## 1.3.1
+
+* fix fashion mnist
+
 ## 1.3.0
 
 * add number of example param in downloader
--- image_embeddings/version.py
+++ image_embeddings/version.py
@@ -1,4 +1,4 @@
 # pylint: disable=all
 
-__version__ = "1.3.0"
+__version__ = "1.3.1"
 __author__ = "Romain Beaumont"
